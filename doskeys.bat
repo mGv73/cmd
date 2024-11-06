6bcc1ea10c32c491fdff0c55f6db705bf227ef6f
mGv73 <111271925+mGv73@users.noreply.github.com>
2024-11-06 20:41:24 +0100

    Update doskeys.bat

--- doskeys.bat
+++ doskeys.bat
@@ -2,5 +2,5 @@ doskey ip=ipconfig /all
 doskey restart=shutdown /r /t 0
 doskey sd=shutdown /s /t 0
 doskey rickroll=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%rickroll.ps1"
-doskey rickroll=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%disablemouse.ps1"
+doskey dmouse=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%disablemouse.ps1"
 for /f %%I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%%I
