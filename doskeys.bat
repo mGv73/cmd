0e2790ab66f95f8d00cf30ad8f45ae08de38f273
mGv73 <111271925+mGv73@users.noreply.github.com>
2024-11-06 19:30:46 +0100

    Update doskeys.bat

--- doskeys.bat
+++ doskeys.bat
@@ -1,4 +1,4 @@
 doskey ip=ipconfig /all
 doskey restart=shutdown /r /t 0
 doskey sd=shutdown /s /t 0
-for /f %I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%I
+for /f %%I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%%I
