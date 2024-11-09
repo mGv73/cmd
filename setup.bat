3f3194c0505c8e61006e5583786db4b96333fff5
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 00:03:19 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
-cls & prompt pikaprompt: & color 06 & echo. & echo   (\__/)^ & echo   (o^-^o) & echo  z(_(")(") & echo. & echo ======================================================================================================================= & echo. & \ 
-echo 1. ip & \
-echo 2. sd & \
-echo 3. restart & \
-echo 4. c & \
-echo 5 (prank). rickroll & \
-echo 6 (prank). dmouse & \
+cls & prompt pikaprompt: & color 06 & echo. & echo   (\__/)^ & echo   (o^-^o) & echo  z(_(")(") & echo. & echo ======================================================================================================================= & echo. & ^ 
+echo 1. ip & ^
+echo 2. sd & ^
+echo 3. restart & ^
+echo 4. c & ^
+echo 5 (prank). rickroll & ^
+echo 6 (prank). dmouse & ^
 echo. & echo =======================================================================================================================
