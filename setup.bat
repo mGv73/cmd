3fdcc7b18531b270bf9b2542961cacecb75ab336
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 00:23:38 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser" & powershell -Command "Set-ExecutionPolicy Bypass"
 powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/mGv73/cmd/main/doskeys.bat -OutFile doskeys.bat"
 doskeys.bat
-powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/mGv73/cmd/main/setup.bat -OutFile setup.bat"
-setup.bat
+powershell -Command "Invoke-WebRequest -Uri https://raw.githubusercontent.com/mGv73/cmd/refs/heads/main/theme.bat -OutFile theme.bat"
+theme.bat
