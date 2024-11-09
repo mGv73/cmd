325ffe50cc6cce4fa370094edf3c279a7e6c8894
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 00:38:49 +0100

    deleted bad files

--- doskeys.bat
+++
@@ -1,7 +0,0 @@
-doskey ip=ipconfig /all
-doskey restart=shutdown /r /t 0
-doskey sd=shutdown /s /t 0
-doskey c=setup.bat
-doskey rickroll=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%rickroll.ps1"
-doskey dmouse=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%disablemouse.ps1"
-for /f %%I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%%I
