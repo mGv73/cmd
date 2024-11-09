39c716b2eb26733a6ef7473158ee5e65052fb73c
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 00:14:30 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,4 +1,9 @@
 @echo off
-echo hello
-echo hi
-echo tjobre
+cls & prompt pikaprompt: & color 06 & echo. & echo   (\__/)^ & echo   (o^-^o) & echo  z(_(")(") & echo. & echo ======================================================================================================================= & echo.
+echo 1. ip
+echo 2. sd
+echo 3. restart
+echo 4. c
+echo 5 (prank). rickroll
+echo 6 (prank). dmouse
+echo. & echo =======================================================================================================================
