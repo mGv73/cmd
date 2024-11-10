49bac71d20c4ace7cc20444a2d8d87c1611f2ea3
mGv73 <nordstrom.max.2010@gmail.com>
2024-11-10 09:56:01 +0100

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,11 +1,11 @@
 @echo off
 powershell -Command "Set-ExecutionPolicy Bypass -Scope CurrentUser" & powershell -Command "Set-ExecutionPolicy Bypass"
-doskey ip=ipconfig /all
-doskey restart=shutdown /r /t 0
-doskey sd=shutdown /s /t 0
 doskey c=setup.bat
-doskey rickroll=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%rickroll.ps1"
+doskey ip=ipconfig /all
+doskey sd=shutdown /s /t 0
+doskey restart=shutdown /r /t 0
 doskey dmouse=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%disablemouse.ps1"
+doskey rickroll=powershell -ExecutionPolicy Bypass -File "%%driveLetter%%rickroll.ps1"
 for /f %%I in ('wmic logicaldisk where "VolumeName='DUCKY'" get DeviceID ^| findstr /r /v "^$"') do set driveLetter=%%I
 cls & prompt pikaprompt: & color 06 & echo. & echo   (\__/)^ & echo   (o^-^o) & echo  z(_(")(") & echo. & echo ======================================================================================================================= & echo.
 echo 1. c
